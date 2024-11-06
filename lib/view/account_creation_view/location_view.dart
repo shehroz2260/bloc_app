7e4b84279f3b3f140f35d65f25631cdd2c3765f8
Shehroz Mukhtar <shehroz2260@gmail.com>
2024-11-06 15:09:45 +0500

    add welcome screen

--- lib/view/account_creation_view/location_view.dart
+++ lib/view/account_creation_view/location_view.dart
@@ -1,3 +1,5 @@
+import 'package:chat_with_bloc/src/app_colors.dart';
+import 'package:chat_with_bloc/src/width_hieght.dart';
 import 'package:chat_with_bloc/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,32 +13,47 @@ class LocationPermissionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18),
-          child: Column(
-            children: [
-              const SizedBox(height: 60),
-              const SizedBox(height: 30),
-              const Text(
-                "Location permissions is required for This App",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-              const Spacer(),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: CustomButton(
-                  onTap: () => context.read<LocationBloc>().add(
-                      OnRequestPermissionEvent(
-                          context: context, isFromOnboard: isFromOnboard)),
-                  btnName: 'Enable Location',
-                ),
-              ),
-              const SizedBox(height: 30),
-            ],
+      body: Column(
+        children: [
+          Image.asset("assets/images/png/pngwing 3.png"),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Image.asset("assets/images/png/address (1).png"),
           ),
-        ),
+          const AppHeight(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Need your location",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: AppColors.redColor,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Please give us access to your Gps Location for better experience to see user nearby you.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomNewButton(
+              onTap: () => context.read<LocationBloc>().add(
+                  OnRequestPermissionEvent(
+                      context: context, isFromOnboard: isFromOnboard)),
+              btnName: 'Enable Location',
+            ),
+          ),
+          const SizedBox(height: 30),
+        ],
       ),
     );
   }
--- lib/view/account_creation_view/welcome_view.dart
+++ lib/view/account_creation_view/welcome_view.dart
@@ -1,8 +1,12 @@
+import 'package:chat_with_bloc/src/app_assets.dart';
 import 'package:chat_with_bloc/src/app_colors.dart';
+import 'package:chat_with_bloc/src/app_text_style.dart';
 import 'package:chat_with_bloc/src/go_file.dart';
+import 'package:chat_with_bloc/src/width_hieght.dart';
 import 'package:chat_with_bloc/view/main_view/main_view.dart';
 import 'package:chat_with_bloc/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({super.key});
@@ -11,16 +15,37 @@ class WelcomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
-      body: Column(
-        children: [
-          const Spacer(),
-          CustomNewButton(
-            btnName: "Continue",
-            onTap: () {
-              Go.offAll(context, const MainView());
-            },
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(AppAssets.appIcon),
+            const AppHeight(height: 40),
+            Text(
+              "Congrats!",
+              style: AppTextStyle.font25.copyWith(color: AppColors.blackColor),
+            ),
+            Text(
+              "You are all set your profile",
+              style: AppTextStyle.font20.copyWith(color: AppColors.blackColor),
+            ),
+            const AppHeight(height: 40),
+            Text(
+              "Now you are ready to explore the peoples and make match with your perfect partener",
+              style: AppTextStyle.font16.copyWith(color: AppColors.blackColor),
+              textAlign: TextAlign.center,
+            ),
+            const AppHeight(height: 40),
+            CustomNewButton(
+              btnName: "Get into App",
+              onTap: () {
+                Go.offAll(context, const MainView());
+              },
+            ),
+            const AppHeight(height: 20)
+          ],
+        ),
       ),
     );
   }
