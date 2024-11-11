2127b20708699e4ee2151534c0891329ff796c00
Shehroz Mukhtar <shehroz2260@gmail.com>
2024-11-11 17:36:59 +0500

    flutter localiZations setup

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'firebase_options.dart';
 import 'view_model/gender_bloc/gender_bloc.dart';
 import 'view_model/location_permission_bloc/location_bloc.dart';
@@ -92,6 +93,11 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       child: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
         child: const MaterialApp(
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
           debugShowCheckedModeBanner: false,
           home: SplashView(),
         ),
